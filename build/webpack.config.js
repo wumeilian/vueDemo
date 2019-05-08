290ff369dfe6c29b99e7ba63cc3864be165fca31
wumeilian <1054527951@qq.com>
2019-05-08 18:32:16 +0800

    add css、html handle

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,4 +1,9 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebapckPlugin = require('clean-webpack-plugin');
+const devMode = process.env.NODE_ENV !== 'production';
+console.log(process.NODE_ENV, 7777888)
 
 module.exports = {
     mode: 'development',
@@ -10,9 +15,9 @@ module.exports = {
         // 配置打包文件输出目录
         path: path.resolve(__dirname, '../dist'),
         // 生成的js文件名称
-        filename: 'js/[name].[hash:8].js',
+        filename: 'js/[name].js',
         // 生成的chunk名称
-        chunkFilename: 'js/[name].[hash:8].js',
+        chunkFilename: 'js/[name].js',
         // 资源引用路径
         publicPath: './'
     },
@@ -26,7 +31,11 @@ module.exports = {
                 test: /\.(scss|sass)$/,
                 use: [
                     {
-                        loader: 'style-loader'
+                        // loader: 'style-loader'
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            hmr: process.env.NODE_ENV === 'development',
+                        }
                     },
                     {
                         loader: 'css-loader',
@@ -43,7 +52,27 @@ module.exports = {
                         loader: 'postcss-loader'
                     }
                 ]
+            }, { // 图片和字体
+                test: /\.(png|svg|jpe?g|icon|gif|eot|ttf|woff2?)$/,
+                exclude: /(node_modules)/,
+                use: [{
+                    loader: 'url-loader',
+                    options: {
+                        limit: 500,
+                        name: 'imgs/[name].[ext]'
+                    }
+                }]
             }
         ]
-    }
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: devMode ? 'css/[name].css' : 'css/[name].[hash].css',
+            chunkFilename: devMode ? 'css/[id].css' : 'css/[id].[hash].css'
+        }),
+        new HtmlWebpackPlugin({
+            title: 'html test'
+        }),
+        new CleanWebapckPlugin()
+    ]
 }
